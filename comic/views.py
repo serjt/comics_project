0b61ac9e1a4748b1bb48d246b62a6171cfcdb1f2
Elzar Esen <elzaresen@outlook.com>
2016-07-22 05:12:52 +0700

    coordinates manager

--- comic/views.py
+++ comic/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.core.context_processors import csrf
 from django.contrib import auth
 from django.shortcuts import render_to_response
+from django.http import JsonResponse
 
 from .models import *
 
@@ -42,4 +43,17 @@ def coordinates(request, image_id):
         'image': ComicsImage.objects.get(id=image_id),
         'coordinates': Coordinate.objects.filter(image_id=image_id)
     }
+    ###############################################################################
+    if request.method == 'POST':
+        # POST goes here . is_ajax is must to capture ajax requests. Beginner's pit.
+        if request.is_ajax():
+            # Always use get on request.POST. Correct way of querying a QueryDict.
+            coordinatez = request.POST.get('coordinates')
+            data = {"coordinates": coordinatez}
+            # Returning same data back to browser.It is not possible with Normal submit
+            print data
+            return JsonResponse(data)
+
+            # Get goes here
+    #############################################################################
     return render_to_response('coordinates.html', context)
